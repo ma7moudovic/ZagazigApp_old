b59e99e050c8547f7875c3313f1a605202767aba
ma7moudovic <ma7moudovic@hotmail.com>
2016-04-26 18:02:07 +0200

    check photo offline exits done

--- app/src/main/java/com/sharkawy/zagazigapp/activities/PhotoActivity.java
+++ app/src/main/java/com/sharkawy/zagazigapp/activities/PhotoActivity.java
@@ -1,6 +1,7 @@
 package com.sharkawy.zagazigapp.activities;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -27,32 +28,61 @@ public class PhotoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_photo);
 
         imageView = (ImageView) findViewById(R.id.imageID);
+        String URL = "http://mashaly.net/places_imgs//icons//29.jpg" ;
+
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/ZagApp/29.jpg");
+        if (myDir.exists()) {
+            // Do Whatever you want sdcard exists
+            Toast.makeText(PhotoActivity.this, "Exists", Toast.LENGTH_SHORT).show();
+            Bitmap bitmap1 = BitmapFactory.decodeFile(myDir.getAbsolutePath());
+            imageView.setImageBitmap(bitmap1);
+
+        }
+        else{
+            Toast.makeText(PhotoActivity.this, "not Exists", Toast.LENGTH_SHORT).show();
+            Picasso.with(this).load(URL).into(new Target() {
+                @Override
+                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                    try {
+                        String root = Environment.getExternalStorageDirectory().toString();
+                        File myDir = new File(root + "/ZagApp");
+                        if (!myDir.exists()) {
+                            myDir.mkdirs();
+                        }
+//                    String name = new Date().toString() + ".jpg";
+                        String name = "29.jpg";
+
+                        myDir = new File(myDir, name);
+                        FileOutputStream out = new FileOutputStream(myDir);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                        out.flush();
+                        out.close();
+                        Toast.makeText(PhotoActivity.this, "imageDownloaded", Toast.LENGTH_SHORT).show();
+                        imageView.setImageBitmap(bitmap);
+
+                    } catch (Exception e) {
+                        Toast.makeText(PhotoActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+
+                @Override
+                public void onBitmapFailed(Drawable errorDrawable) {
+                }
+
+                @Override
+                public void onPrepareLoad(Drawable placeHolderDrawable) {
+                }
+            });
+        }
+
+
+
         if(getIntent()!=null&&getIntent().getExtras()!=null){
             if(getIntent().getExtras().containsKey(EXTRA_IMAGE)){
-                Picasso.with(this).load("http://mashaly.net/"+getIntent().getExtras().getString(EXTRA_IMAGE)).into(new Target() {
-                    @Override
-                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                        try {
-                            String root = Environment.getExternalStorageDirectory().toString();
-                            File myDir = new File(root + "/ZagApp");
-                            if (!myDir.exists()) {
-                                myDir.mkdirs();
-                            }
-                            String name = new Date().toString() + ".jpg";
-                            myDir = new File(myDir, name);
-                            FileOutputStream out = new FileOutputStream(myDir);
-                            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
-                            out.flush();
-                            out.close();
-                            Toast.makeText(PhotoActivity.this,"imageDownloaded",Toast.LENGTH_SHORT).show();
-                        } catch(Exception e){}
-                    }
+//                String URL = "http://mashaly.net/"+getIntent().getExtras().getString(EXTRA_IMAGE) ;
 
-                    @Override
-                    public void onBitmapFailed(Drawable errorDrawable) {}
-                    @Override
-                    public void onPrepareLoad(Drawable placeHolderDrawable) {}
-                });
             }
         }
     }
